a427fa84b1eb9ccfaa5437f5f31129a69dbc5ae1
Ben Thackray <mthack92@gmail.com>
2021-06-17 11:51:00 -0400

    added randomization to all multiplechoice questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -130,14 +130,11 @@ fetch(triviaUrl)
     let readyQuestionTwoOptionC = data.results[0].correct_answer.replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     let readyQuestionTwoOptionD = data.results[0].incorrect_answers[2].replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     
+    // put all choices into an array
     let q2choices = [readyQuestionTwoOptionA, readyQuestionTwoOptionB, readyQuestionTwoOptionC, readyQuestionTwoOptionD];
     console.log(q2choices);
-
-    // function shuffle(array) {
-    //     array.sort(() => Math.random() - 0.5);
-    //   }
+    // randomize choices
     q2choices = q2choices.sort(() => Math.random() - 0.5);
-    // shuffle(q2choices)
 
     console.log(q2choices);
 
@@ -154,11 +151,19 @@ fetch(triviaUrl)
     let readyQuestionFourOptionC = data.results[1].incorrect_answers[1].replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     let readyQuestionFourOptionD = data.results[1].incorrect_answers[2].replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     
+        // put all choices into an array
+        let q4choices = [readyQuestionFourOptionA, readyQuestionFourOptionB, readyQuestionFourOptionC, readyQuestionFourOptionD];
+        console.log(q4choices);
+        // randomize choices
+        q4choices = q4choices.sort(() => Math.random() - 0.5);
+    
+        console.log(q4choices);
+
     questionFour.text(readyQuestionFour);
-    questionFourOptionA.text(readyQuestionFourOptionA);
-    questionFourOptionB.text(readyQuestionFourOptionB);
-    questionFourOptionC.text(readyQuestionFourOptionC);
-    questionFourOptionD.text(readyQuestionFourOptionD);
+    questionFourOptionA.text(q4choices[0]);
+    questionFourOptionB.text(q4choices[1]);
+    questionFourOptionC.text(q4choices[2]);
+    questionFourOptionD.text(q4choices[3]);
 
     //question 6
     let readyQuestionSix = data.results[2].question.replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
@@ -167,11 +172,19 @@ fetch(triviaUrl)
     let readyQuestionSixOptionC = data.results[2].incorrect_answers[1].replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     let readyQuestionSixOptionD = data.results[2].correct_answer.replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     
+        // put all choices into an array
+        let q6choices = [readyQuestionSixOptionA, readyQuestionSixOptionB, readyQuestionSixOptionC, readyQuestionSixOptionD];
+        console.log(q6choices);
+        // randomize choices
+        q6choices = q6choices.sort(() => Math.random() - 0.5);
+    
+        console.log(q6choices);
+
     questionSix.text(readyQuestionSix);
-    questionSixOptionA.text(readyQuestionSixOptionA);
-    questionSixOptionB.text(readyQuestionSixOptionB);
-    questionSixOptionC.text(readyQuestionSixOptionC);
-    questionSixOptionD.text(readyQuestionSixOptionD);
+    questionSixOptionA.text(q6choices[0]);
+    questionSixOptionB.text(q6choices[1]);
+    questionSixOptionC.text(q6choices[2]);
+    questionSixOptionD.text(q6choices[3]);
 
     //question 8
     let readyQuestionEight = data.results[3].question.replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
@@ -180,11 +193,19 @@ fetch(triviaUrl)
     let readyQuestionEightOptionC = data.results[3].incorrect_answers[1].replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     let readyQuestionEightOptionD = data.results[3].incorrect_answers[2].replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     
+        // put all choices into an array
+        let q8choices = [readyQuestionEightOptionA, readyQuestionEightOptionB, readyQuestionEightOptionC, readyQuestionEightOptionD];
+        console.log(q8choices);
+        // randomize choices
+        q8choices = q8choices.sort(() => Math.random() - 0.5);
+    
+        console.log(q8choices);
+
     questionEight.text(readyQuestionEight);
-    questionEightOptionA.text(readyQuestionEightOptionA);
-    questionEightOptionB.text(readyQuestionEightOptionB);
-    questionEightOptionC.text(readyQuestionEightOptionC);
-    questionEightOptionD.text(readyQuestionEightOptionD);
+    questionEightOptionA.text(q8choices[0]);
+    questionEightOptionB.text(q8choices[1]);
+    questionEightOptionC.text(q8choices[2]);
+    questionEightOptionD.text(q8choices[3]);
 
     //question 9
     let readyQuestionNine = data.results[4].question.replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
@@ -193,11 +214,19 @@ fetch(triviaUrl)
     let readyQuestionNineOptionC = data.results[4].incorrect_answers[1].replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     let readyQuestionNineOptionD = data.results[4].incorrect_answers[2].replaceAll('%20', ' ').replaceAll('%3A%20', ' ').replaceAll('%3F', ' ').replaceAll('%27s%201989%20', ' ').replaceAll('%20%22', ' ').replaceAll('%22', ' ').replaceAll('%28', ' ').replaceAll('%29', ' ').replaceAll('%27', ' ').replaceAll('%60', ' ').replaceAll('%2C', '"').replaceAll('%21', '"').replaceAll('%26', '&').replaceAll('%3A', ' ').replaceAll('%2', ' ').replaceAll('%C3%B1%C3%', ' ');
     
+        // put all choices into an array
+        let q9choices = [readyQuestionNineOptionA, readyQuestionNineOptionB, readyQuestionNineOptionC, readyQuestionNineOptionD];
+        console.log(q9choices);
+        // randomize choices
+        q9choices = q9choices.sort(() => Math.random() - 0.5);
+    
+        console.log(q9choices);
+
     questionNine.text(readyQuestionNine);
-    questionNineOptionA.text(readyQuestionNineOptionA);
-    questionNineOptionB.text(readyQuestionNineOptionB);
-    questionNineOptionC.text(readyQuestionNineOptionC);
-    questionNineOptionD.text(readyQuestionNineOptionD);
+    questionNineOptionA.text(q9choices[0]);
+    questionNineOptionB.text(q9choices[1]);
+    questionNineOptionC.text(q9choices[2]);
+    questionNineOptionD.text(q9choices[3]);
 
  
     //create fetch with true or false url and do remaining questions
